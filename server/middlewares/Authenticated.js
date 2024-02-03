844d33a1576e9fac7db7d3d6212f8951110035f5
Sahil <garg07825@gmail.com>
2024-02-04 04:52:47 +0530

    added middleware for authentiation and for error

---
+++ server/middlewares/Authenticated.js
@@ -0,0 +1,26 @@
+import User from "../model/User.js";
+import jwt from "jsonwebtoken";
+
+const isAuthenticated = async (req, res, next) => {
+  const token = req.headers.authorization;
+
+  if (!token) {
+    return res.status(401).json({
+      success: false,
+      message: "Unauthorized: Token not provided",
+    });
+  }
+
+  try {
+    const decodedData = jwt.verify(token, process.env.jwt_Secret);
+    req.user = await User.findById(decodedData._id);
+    next();
+  } catch (error) {
+    return res.status(401).json({
+      success: false,
+      message: "Unauthorized: Invalid token",
+    });
+  }
+};
+
+export default isAuthenticated;
