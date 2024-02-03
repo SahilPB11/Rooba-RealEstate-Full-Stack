ed36f0e180b1818ddf663abb2d1b20e4e8fae967
Sahil <garg07825@gmail.com>
2024-02-04 04:26:26 +0530

    added a important iddlewares are important

--- server/index.js
+++ server/index.js
@@ -1,4 +1,18 @@
 import express from "express";
+import { config } from "dotenv";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
 
 // here i am exporting app to server.js file
 export const app = express();
+
+// config a env path
+config({
+  path: "./.env",
+});
+
+// middlewares
+app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cookieParser());
